2112e1a7f4457c933ddcc8c9674c74e3c856a82d
Lorraineken <lorrainekupa@gmail.com>
2023-02-09 15:36:47 +0300

    completed assignment

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  [1,2,3,4]
+end
+
+def add_element_to_end_of_array(array=[2,3,4], element=6)
+   array.push(element)
+end
+
+def add_element_to_start_of_array(array=[2,3,4], element=1)
+  array.unshift(element)
+end
+
+def remove_element_from_end_of_array(array=[2,3,4])
+  array.pop
+end
+
+def remove_element_from_start_of_array(array=[2,3,4])
+    array.shift
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def retrieve_element_from_index(array=[2,3,4], index_number=1)
+   array[index_number]
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def retrieve_first_element_from_array(array=[2,3,4])
+  array.first
 end
 
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array(array=[2,3,4])
+    array.last
   
 end
